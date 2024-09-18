d6a244c50ec8a0ed561408b2149fd75a16bb4dec
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-18 09:17:11 -0600

    inital commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Gwen Thelin on 9/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
